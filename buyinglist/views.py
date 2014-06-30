4335b8ecc6ea5f022f443ba850bb6d552c3eb8e8
Niaden <damir_ne_@mail.ru>
2014-06-30 19:28:00 +0300

    view.py. localhost/tobuy/ is showing some useless info now. i'm almost happy

--- buyinglist/views.py
+++ buyinglist/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from buyinglist.models import Item
 
 # Create your views here.
+
+def tobuy(request):
+	view = "some text"
+	html = "<html><body><h3>%s</h3></html></body>" % view
+	return HttpResponse(html)
