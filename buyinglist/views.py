b0f8180c40ef5efbe4f00ea451e27cc6aec9d6c1
Niaden <damir_ne_@mail.ru>
2014-07-01 11:19:57 +0300

    buyinglist/views.py and lesson1/settings.py. show info in db like cells in tables

--- buyinglist/views.py
+++ buyinglist/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
+from django.shortcuts import render_to_response
 from buyinglist.models import Item
 
 # Create your views here.
 
+# def tobuy(request):
+# 	view = "some text"
+# 	html = "<html><body><h3>%s</h3></html></body>" % view
+# 	return HttpResponse(html)
+
 def tobuy(request):
-	view = "some text"
-	html = "<html><body><h3>%s</h3></html></body>" % view
-	return HttpResponse(html)
+	return render_to_response('tobuy.html',{'items': Item.objects.all()})
--- lesson1/settings.py
+++ lesson1/settings.py
@@ -26,6 +26,9 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+TEMPLATE_DIRS = (
+    '/home/damir/djangoenvstudiyng/bin/lesson1/templates'
+)
 
 # Application definition
 
